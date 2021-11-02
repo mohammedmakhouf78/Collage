cb5b641a59966daa5c7e34506f1de685b409ae22
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2021-11-02 19:16:23 +0200

    session 1

---
+++ practice/session1.php
@@ -0,0 +1,64 @@
+<?php
+
+//انا بطبع هنا اسمي
+echo "<span style='color:red'> Mohammed </span>";
+
+echo "<br>";
+
+Echo "<span style='color:red'> Ahmed </span>";
+echo "<br>";
+
+$Name = "ahmed mohammed esmail";
+$address = 'Giza';
+
+// echo "hello " . $Name;
+echo 'hello' . $Name . ' your address is'. $address;
+echo "<br>";
+
+
+// echo ucwords($Name);
+
+// city , country,street ==> address
+// $city = "كفر الشيخ";
+// $country = "مصر ";
+// $street = "النبوي المهندس";
+// $address = "";
+// $address .=$city ;
+// $address .=$country ;
+// $address .=$street ;
+// echo $address;
+
+// echo strlen("asfsadf");
+
+
+
+
+
+
+// $id = 15; // int
+// // $id = 15.5; //floot
+
+
+
+// echo 10 + 5 * 9 + 19 -20;
+
+
+
+
+
+
+// quantity,price,tax = 5% ====> total
+
+// echo 50 * 20 * 0.05;
+
+
+$b = 10;
+
+if($b != 9)
+{
+    echo "true";
+}
+else
+{
+    echo "false";
+}
